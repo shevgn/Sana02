ae6cae9cab6df794394f64a89cdcd91675b1cbd8
Artem Levchenko <evgn.main@gmail.com>
2023-11-23 20:26:30 +0200

    While1

---
+++ Sana02/While1/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter n:");
+var n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("a = " + A(n));
+Console.WriteLine("b = " + B(n));
+Console.WriteLine("c = " + C(n));
+
+double A(int num)
+{
+    double sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += Math.Pow(-1, i) / (2*i + 1);
+    }
+
+    return sum;
+}
+
+double B(int num)
+{
+    double sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += 1 + 1 / Math.Pow(i, 2);
+    }
+
+    return sum;
+}
+
+int C(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        int fact = 1;
+        for (int j = i; j > 1; j--)
+        {
+            fact *= j;
+        }
+        sum += fact;
+    }
+
+    return sum;
+}
